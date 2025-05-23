f610af82b4cf2dd84b4c8254e9849e210f7344f8
Akash Chandra <akash.chandra.4u@gmail.com>
2025-05-24 02:25:51 +0530

    feat: update HUD message to include health gain instructions

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -9,7 +9,7 @@ pub fn hud(ecs: &SubWorld) {
 
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
-    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
+    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move. Space to Gain Health (skips turn).");
     draw_batch.bar_horizontal(
         Point::zero(),
         SCREEN_WIDTH * 2,
